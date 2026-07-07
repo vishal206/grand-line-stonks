f464b64160a5dde016d9b808f167d7729069ecd0
vishal206 <vishal206ani@gmail.com>
2026-07-07 16:03:55 +0530

    feat: add catch-all error mappings and request id logging

--- backend/src/main/java/com/grandlinestonks/error/ApiExceptionHandler.java
+++ backend/src/main/java/com/grandlinestonks/error/ApiExceptionHandler.java
@@ -2,14 +2,21 @@ package com.grandlinestonks.error;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -78,5 +85,24 @@ public class ApiExceptionHandler {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ApiError handleNotMarketOwner(NotMarketOwnerException e) {
         return ApiError.of("NOT_MARKET_OWNER", e.getMessage());
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentTypeMismatchException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleUnreadableRequest(Exception e) {
+        return ApiError.of("MALFORMED_REQUEST", "request could not be parsed");
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleNoResource(NoResourceFoundException e) {
+        return ApiError.of("NOT_FOUND", "no such resource");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleUnexpected(Exception e) {
+        log.error("unhandled exception", e);
+        return ApiError.of("INTERNAL_ERROR", "an unexpected error occurred");
     }
 }
